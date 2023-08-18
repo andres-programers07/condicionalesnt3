74967224411fbd890a8d2502cad699ba93e8a39e
andres mosquera <ormoca2009@hotmail.com>
2023-08-18 10:15:25 -0500

    nt3 se inicia condicionales

---
+++ condiciones.py
@@ -0,0 +1,7 @@
+temperatura = int(input("digita la temperatura"))
+gradosdealcohol = int(input("ingrese su grado de alcohol"))
+
+if temperatura > 50 or gradosdealcohol > 5:
+    print("alarma")
+else:
+    print("normal")
